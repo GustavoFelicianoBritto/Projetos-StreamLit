1afdd3bbf4f514af5456220787d2bf9015a99e35
GustavoFelicianoBritto <gustavofelicianoemail@gmail.com>
2025-05-06 12:01:48 -0300

    Espaço para testes, protótipos, Debugs

---
+++ Debug.py
@@ -0,0 +1,15 @@
+import streamlit as st
+# from streamlit import session_state
+#
+# if st.session_state.login not in session_state:
+#     st.session_state.login=False
+#
+# if not st.session_state.login:
+#     st.text_input("Digite seu novo nome de usuario: ")
+#     if st.button("Register"):
+#         st.session_state.login=True
+
+
+
+
+
